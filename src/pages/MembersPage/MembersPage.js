2601d541fa58fad44ac535af7f7924bd94737d7d
Jorge Herrera <jrgherrera@gmail.com>
2021-09-02 19:10:01 -0600

    GH-12 Add ability to edit members in members page

--- src/pages/MembersPage/MembersPage.js
+++ src/pages/MembersPage/MembersPage.js
@@ -19,7 +19,9 @@ const MembersPage = () => {
   const auth = useAuth();
 
   const [memberData, setMemberData] = useState(memberTemplate);
+  const [memberModalData, setMemberModalData] = useState(memberTemplate);
   const [isLoading, setIsLoading] = useState(false);
+  const [isUpdateModalActive, setIsUpdateModalActive] = useState(false);
 
   const onInfoChange = e => {
     const el = e.currentTarget;
@@ -39,6 +41,32 @@ const MembersPage = () => {
 
   const onMemberDelete = id =>
     memberHook.remove(id)
+
+  const onToggleModal = id => {
+    const isModalOpen = !isUpdateModalActive;
+    setIsUpdateModalActive(isModalOpen)
+    if (isModalOpen) {
+      const currentMemberData = memberHook.get(id);
+      setMemberModalData({ ...currentMemberData })
+    }
+  }
+
+  const onModalValueChange = e => {
+    const el = e.currentTarget;
+    setMemberModalData({
+      ...memberModalData,
+      [el.dataset.type]: el.value
+    })
+  }
+
+  const onSaveMember = () => {
+    setIsLoading(true);
+    memberHook.update(memberModalData)
+      .then(() => {
+        setIsLoading(false)
+        onToggleModal();
+      })
+  }
 
   return <>
     {isLoading && <Spinner screen />}
@@ -147,8 +175,13 @@ const MembersPage = () => {
                       <td>{member.phone || '---'}</td>
                       <td>{member.email || '---'}</td>
                       <td>{member.birthday || '---'}</td>
-                      <td>
-                        <button className="button is-small" onClick={() => onMemberDelete(member.id)}>
+                      <td className="actions-column">
+                        <button className="button is-small is-info" onClick={() => onToggleModal(member.id)}>
+                          <span className="icon is-small">
+                            <i className="fas fa-pen"></i>
+                          </span>
+                        </button>
+                        <button className="button is-small is-danger" onClick={() => onMemberDelete(member.id)}>
                           <span className="icon is-small">
                             <i className="fas fa-trash"></i>
                           </span>
@@ -163,6 +196,87 @@ const MembersPage = () => {
         </div>
       </div>
     </div>;
+    <div className={`modal${isUpdateModalActive ? ' is-active' : ''}`}>
+      <div className="modal-background"></div>
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">Actualizar datos de miembro</p>
+          <button className="delete" aria-label="close" onClick={onToggleModal}></button>
+        </header>
+        <section className="modal-card-body">
+          <div className="columns">
+            <div className="column is-half">
+              <div className="block">
+                <div className="field">
+                  <label className="label">Nombre</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="Título"
+                      data-type="name"
+                      value={memberModalData.name}
+                      onChange={onModalValueChange}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="block">
+                <div className="field">
+                  <label className="label">Email</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="email"
+                      placeholder="Correo electrónico"
+                      data-type="email"
+                      value={memberModalData.email}
+                      onChange={onModalValueChange}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="column is-half">
+              <div className="block">
+                <div className="field">
+                  <label className="label">Teléfono</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="phone"
+                      placeholder="Teléfono"
+                      data-type="phone"
+                      value={memberModalData.phone}
+                      onChange={onModalValueChange}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="block">
+                <div className="field">
+                  <label className="label">Fecha de nacimiento</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="date"
+                      placeholder="Fecha de nacimiento"
+                      data-type="birthday"
+                      value={memberModalData.birthday}
+                      onChange={onModalValueChange}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <footer className="modal-card-foot">
+          <button className="button is-success" onClick={onSaveMember}>Actualizar Miembro</button>
+          <button className="button" onClick={onToggleModal}>Cancelar</button>
+        </footer>
+      </div>
+    </div>
   </>
 }
 
