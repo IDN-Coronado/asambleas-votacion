d3ab1d80edb93645b6405e3320a4a34ba17a144d
Jorge Herrera <jrgherrera@gmail.com>
2021-09-28 19:54:23 -0600

    Copy change

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -8,7 +8,7 @@ const HomePage = () => {
 		<div className="container">
 			{!auth.user.church && <div className="columns mt-2">
 				<div className="column is-12">
-					<p className="subtitle">Por favor, contacta al encargado de tu iglesia para que puedas manejar asambleas de la misma.</p>
+					<p className="subtitle">Por favor, contacta al <a href="mailto:jrgherrera@gmail.com">administrador del sitio</a> para que pueda asignarte una iglesia.</p>
 				</div>
 			</div>}
 		</div>
